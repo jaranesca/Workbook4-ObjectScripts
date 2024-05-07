5dadefa098b8d8e124373f6199db634ef9460ed6
Jaranesca Braker <142855048+jaranesca@users.noreply.github.com>
2024-05-07 11:40:19 -0700

    prooduct.js
    code for products

---
+++ products.js
@@ -0,0 +1,21 @@
+"use strict";
+
+let products = [
+    {
+        name: 'Globe Noir',
+        description: 'A beautiful black globe.',
+        cost: 199.99
+    },
+    {
+        name: 'Ancient Globe',
+        description: 'A beautiful ancient globe.',
+        cost: 199.99
+    },
+    {
+        name:'Brown Tone Globe',
+        description: 'A beautiful brown globe.',
+        cost: 199.99
+    }
+];
+
+
---
+++ users.js
@@ -0,0 +1,34 @@
+"use strict";
+
+let students = [
+    {
+        "name": "Jaranesca Braker",
+        "group": "App Dev Team",
+        "github": "https://github.com/JaranescaBraker",
+        "linkedin": "https://www.linkedin.com",
+        "email": "jbraker@appdev.yearup.org",
+        "nickname": "JSJ",
+        "imageurl": "img/Avatar_Student_Male.jpg",
+        "gender": "female"
+    },
+    {
+        "name": "Maximo Mayo",
+        "group": "App Dev Team",
+        "github": "https://github.com/MaximoMayo",
+        "linkedin": "https://www.linkedin.com/in/maximo-mayo-a2ba10252/?original_referer=",
+        "email": "mmayo@appdev.yearup.org",
+        "nickname": "reference error",
+        "imageurl": "img/Avatar_Student_Male.jpg",
+        "gender": "male"
+    },
+    {
+        "name": "Maximo Mayo",
+        "group": "App Dev Team",
+        "github": "https://github.com/MaximoMayo",
+        "linkedin": "https://www.linkedin.com",
+        "email": "mmayo@appdev.yearup.org",
+        "nickname": "reference error",
+        "imageurl": "img/Avatar_Student_Male.jpg",
+        "gender": "male"
+    }
+]
