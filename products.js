58d94087c83f6804e7c7394a16baa61717d7231f
Jaranesca Braker <142855048+jaranesca@users.noreply.github.com>
2024-05-07 13:53:23 -0700

    update product.js

--- products.js
+++ products.js
@@ -18,4 +18,41 @@ let products = [
     }
 ];
 
+console.log(`The ${products[2].name} is ${products[2].description} which cost $${products[2].cost}`)
 
+
+let book = {
+    title: "Eat that Frog",
+    price: 19.95,
+    displayInformation: function() {
+        return `Title: ${this.title} and price ${this.price}`
+    }
+}
+
+console.log(book.displayInformation());
+
+let person = {
+    firstname: 'Jaranesca',
+    lastname: 'Braker',
+    fullname: function() {
+        //this outputs it
+        console.log(`${this.firstname} ${this.lastname}`)
+    }
+    
+}
+person.fullname();
+
+console.log("Hi from person 1 the fullname is " + person.fullname())
+
+let person2 = {
+    firstname: 'Jaranesca',
+    lastname: 'Braker',
+    fullname: function() {
+        //this outputs it
+        return `${this.firstname} ${this.lastname}`;
+    }
+    
+}
+
+let savedTheFullName = person2.fullname();
+console.log("Hi from person 1 the fullname is " + savedTheFullName)
