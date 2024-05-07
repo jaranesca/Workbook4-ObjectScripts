a51c1a628fee9c375cbffa3ea0902e4fbd908d6c
Jaranesca Braker <142855048+jaranesca@users.noreply.github.com>
2024-05-07 08:48:41 -0700

    first object

---
+++ label_maker.js
@@ -0,0 +1,11 @@
+let myObject={
+    firstName:"Jaranesca",
+    lastName:"Braker",
+    age: 21,
+    email:"bjaranesca@gmail.com"
+}
+
+console.log(myObject.email)
+console.log(myObject.firstName)
+console.log(myObject.lastName)
+console.log(myObject.age)
